2ec6158aeda701c2aefc31df3855d02c226831a8
kidusshoa <captainkevin2008@gmail.com>
2024-11-21 20:44:25 +0300

    routes

--- README.md
+++ README.md
@@ -3,6 +3,34 @@
 simple django rest-framework app
 Book api
 
+# for virtual enviroment
+
+```bash
+pipenv shell
+```
+
+# Installation
+
+```bash
+pipenv install django
+
+pipenv install djangorestframework
+```
+
+# Or for global
+
+```bash
+pip install django
+    or
+sudo apt install python3-django
+```
+
+```bash
+pip install djangorestframework
+```
+
+# Start development
+
 ```bash
 django-admin startproject base
 ```
--- base/urls.py
+++ base/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from books.views import BookViewSet
 
+router = routers.DefaultRouter()
+router.register(r'books', BookViewSet)
+
+# Define API endpoints and Django admin interface
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
